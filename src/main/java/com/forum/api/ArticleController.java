1bb58cfb4acbf2988fadbc246332eebc9514baf9
cry0x <kraemerj.schmelz@outlook.de>
2021-11-16 00:31:18 +0100

    added article structure

---
+++ src/main/java/com/forum/api/ArticleController.java
@@ -0,0 +1,18 @@
+package com.forum.api;
+
+import com.forum.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/articles")
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+}
