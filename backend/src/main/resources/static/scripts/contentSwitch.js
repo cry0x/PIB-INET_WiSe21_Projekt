708145efb1127f5a65afd79d4c56de3cd2f6a438
MrKumata <n.Welsch@protonmail.com>
2021-12-05 07:34:03 +0100

    fix(contentSwitch)Removed Overlap

--- backend/src/main/resources/static/scripts/contentSwitch.js
+++ backend/src/main/resources/static/scripts/contentSwitch.js
@@ -5,8 +5,8 @@ const contact = $('#contact')
 // Switch between main content pages
 function contentSwitch(element) {
     // hides all child elements of the parent element
-    // added the optional speed parameter 'slow' for an animation
-    $('#content').children().hide('slow')
+    $('#content').children().hide()
     // shows the child inside the function argument
+    // added the optional speed parameter 'slow' for an animation
     element.show('slow')
 }
