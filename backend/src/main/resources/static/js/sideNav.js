8cdc1683cec9c18d4dbc76ed6825ca370400cad2
MrKumata <n.Welsch@protonmail.com>
2021-12-10 11:03:46 +0100

    style(index, sideNav.js)Added Styling

--- backend/src/main/resources/static/js/sideNav.js
+++ backend/src/main/resources/static/js/sideNav.js
@@ -1,13 +1,5 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById('sideNav').style.display = 'block'
-    document.getElementById('sideNav').style.width = '250px'
-    document.getElementById('sideNavButton').style.marginRight = '250px'
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById('sideNav').style.display = 'none'
-    document.getElementById('sideNav').style.width = '0'
-    document.getElementById('sideNavButton').style.marginRight = '0'
-}
+$(document).ready(() => {
+    $('#sideNavButton').click(() => {
+        $('#sideNav').toggle()
+    })
+})
