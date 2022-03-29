a3179727bc237d520599fcf4b21c51c0289a6297
jens_k <37385066+cry0x@users.noreply.github.com>
2022-03-29 16:56:42 +0200

    refactoring: new project structure and docker build (#11)
    * dockerbuild
    
    * dockerbuild
    
    * fixed
    
    * compose
    
    * dockerbuild
    
    * updated
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ src/main/resources/static/js/user-management/user-management.js
@@ -0,0 +1,51 @@
+'use strict';
+
+class UserManagement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      dataIsLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    const url = `${window.location.origin}/api/users`;
+    fetch(url).then(res => res.json()).then(json => {
+      this.setState({
+        users: json,
+        DataisLoaded: true
+      });
+    });
+  }
+
+  deleteUser(id) {
+    const url = `${window.location.origin}/api/users/${id}`;
+    const fetchInit = {
+      method: 'DELETE'
+    };
+    fetch(url, fetchInit).then(res => res.json()).then(res => console.log(res));
+  }
+
+  renderTableData() {
+    return this.state.users.map((user, index) => {
+      return /*#__PURE__*/React.createElement("tr", {
+        key: user.id
+      }, /*#__PURE__*/React.createElement("td", null, user.id), /*#__PURE__*/React.createElement("td", null, user.loginname), /*#__PURE__*/React.createElement("td", null, user.firstname), /*#__PURE__*/React.createElement("td", null, user.lastname), /*#__PURE__*/React.createElement("td", null, user.email), /*#__PURE__*/React.createElement("td", {
+        id: "deleteUser"
+      }, /*#__PURE__*/React.createElement("button", {
+        id: "btnDeleteUser",
+        onClick: () => this.deleteUser(user.id)
+      }, "DELETE")));
+    });
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("table", {
+      id: "students"
+    }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Loginname"), /*#__PURE__*/React.createElement("th", null, "Vorname"), /*#__PURE__*/React.createElement("th", null, "Nachname"), /*#__PURE__*/React.createElement("th", null, "EMail"))), /*#__PURE__*/React.createElement("tbody", null, this.renderTableData()));
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(UserManagement, null), document.getElementById('user-management'));
