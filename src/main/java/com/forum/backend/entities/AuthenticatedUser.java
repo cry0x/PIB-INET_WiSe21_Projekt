89a1b76c0fcdfdb9f01afb183f3d87753dbc7eaa
cry0x <kraemerj.schmelz@outlook.de>
2022-03-30 19:32:33 +0200

    fixed everything

--- src/main/java/com/forum/backend/entities/AuthenticatedUser.java
+++ src/main/java/com/forum/backend/entities/AuthenticatedUser.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
